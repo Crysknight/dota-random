1a6d4bf4dd10b67ff61347d6b2e42e25d43e6e38
Paul Rukavishnikov <ad.dolorem@gmail.com>
2020-11-29 16:07:36 +0300

    hey

--- src/store/items/getters.js
+++ src/store/items/getters.js
@@ -1,4 +1,8 @@
-import { NEEDLESS_STARTER_ITEMS } from '@/constants';
+import {
+  NEEDLESS_STARTER_ITEMS,
+  BUILD_ITEMS,
+  BOOTS,
+} from '@/constants';
 
 export default {
   starterItems({ items }) {
@@ -8,5 +12,18 @@ export default {
         !NEEDLESS_STARTER_ITEMS.includes(name)
       );
     });
+  },
+  buildItems: ({ items }, _g, { route }) => (withBoots = true) => {
+    const buildItemsCollection = items.filter(({ name }) => {
+      return (
+        BUILD_ITEMS.includes(name) ||
+        (withBoots && BOOTS.includes(name))
+      );
+    });
+
+    const { seed } = route.params;
+  },
+  boots({ items }) {
+    return items.filter(({ name }) => BOOTS.includes(name));
   },
 };
